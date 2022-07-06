fadfbca2ce24d07e4141bf564264c84cf0c41755
Daniel <daniel@exnaton.com>
2022-07-06 14:23:37 +0200

    remove controller

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PetsService } from './services/pets.service';
-import { PetsController } from './controllers/pets.controller';
 import { PetsResolver } from './resolvers/pets.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from './entities/pet.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pet])],
-  controllers: [PetsController],
   providers: [PetsService, PetsResolver],
 })
 export class PetsModule {}
