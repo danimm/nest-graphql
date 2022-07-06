4ebf881860a5e0490a7329903d37a63742bcae9a
Daniel <daniel@exnaton.com>
2022-07-06 14:06:52 +0200

    create query and mutations from sqlite

--- src/pets/resolvers/pets.resolver.ts
+++ src/pets/resolvers/pets.resolver.ts
@@ -1,6 +1,7 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { PetsService } from '../services/pets.service';
 import { Pet } from '../entities/pet.entity';
+import { CreatePetInput } from '../dtos/pets.dto';
 
 @Resolver((of) => Pet)
 export class PetsResolver {
@@ -9,5 +10,12 @@ export class PetsResolver {
   @Query((returns) => [Pet])
   async pets(): Promise<Pet[]> {
     return await this.petsService.findAll();
+  }
+
+  @Mutation((returns) => Pet)
+  async createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<Pet> {
+    return this.petsService.createPet(createPetInput);
   }
 }
--- src/pets/services/pets.service.ts
+++ src/pets/services/pets.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { Pet } from '../entities/pet.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreatePetInput } from '../dtos/pets.dto';
 
 @Injectable()
 export class PetsService {
   constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
+
   async findAll(): Promise<Pet[]> {
-    const pet = new Pet();
-    pet.id = 1;
-    pet.name = 'Mambo';
-    return [pet];
+    return this.petsRepository.find();
+  }
+
+  async createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    const newPet = this.petsRepository.create(createPetInput);
+    return this.petsRepository.save(newPet);
   }
 }
