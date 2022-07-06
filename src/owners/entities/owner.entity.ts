62b829c16bfd52632b0595cc9c1a8cf51dbc09de
Daniel <daniel@exnaton.com>
2022-07-06 14:35:11 +0200

    create owners + relationship

--- src/owners/entities/owner.entity.ts
+++ src/owners/entities/owner.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Pet } from '../../pets/entities/pet.entity';
 
+@Entity()
 @ObjectType()
 export class Owner {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany(() => Pet, (pet) => pet.owner)
+  @Field((type) => [Pet], { nullable: true })
+  pets?: Pet[];
 }
