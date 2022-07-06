f9dc4bb77549e9d3a6f261318a3ffe625ac6674c
Daniel <daniel@exnaton.com>
2022-07-06 12:11:44 +0200

    resolver, service and entity created

--- src/pets/resolvers/pets.resolver.ts
+++ src/pets/resolvers/pets.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+import { PetsService } from '../services/pets.service';
+import { Pet } from '../entities/pet.entity';
 
-@Resolver()
-export class PetsResolver {}
+@Resolver((of) => Pet)
+export class PetsResolver {
+  constructor(private petsService: PetsService) {}
+
+  @Query((returns) => [Pet])
+  async pets(): Promise<Pet[]> {
+    return await this.petsService.findAll();
+  }
+}
